27bfe1ceff3c8456470d33b01be37938e6df68f1
Vidya Narayanan <vidyan@cmu.edu>
2019-02-14 17:11:53 -0500

    tiny loom driver and sample file

---
+++ jacq3.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import serial
+import time
+def toBits(arr):
+    out = []
+    arr = arr + []
+    while len(arr) % 7 > 0:
+        arr.append(False)
+        while len(arr) >= 7:
+            out.append( arr[0] << 6 | arr[1] << 5 | arr[2] << 4 | arr[3] << 3 | arr[4] <<  2 | arr[5] << 1 | arr[6] )
+            arr = arr[7:]
+    assert(len(arr) == 0)
+    return bytearray(out)
+
+class Jacq3:
+    def __init__(self):
+        self.frames = [False] * 360
+
+    def length(self):
+        return len(self.frames)
+
+    def clearFrame(self):
+        self.frames = [False] * 360
+
+    def setPick(self, index, is_up):
+        self.frames[index] = is_up
+
+    def getPick(self):
+        pick = b'\x80' + toBits(self.frames[0:120]) + b'\xc0' + b'\x81' + toBits(self.frames[120:240]) +       b'\xc0' + b'\x82'+toBits(self.frames[240:360])+b'\xc0';
+        return pick
+
+    def getNullPick(self):
+        pick = b'\x80' + toBits([False]*120) + b'\xc0' +  b'\x81' + toBits([False]*120) +  b'\xc0' +  b'\x82' + toBits([False]*120) + b'\xc0';
+        return pick
+
+
+class Comm:
+    # better port identification
+    def __init__(self):
+        self.port = "/dev/cu.usbserial"
+        self.serial =  serial.Serial(port=self.port, baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+
+    def initialize(self):
+        if self.serial.isOpen() == False:
+            return False
+        self.serial.reset_input_buffer()
+        self.serial.reset_output_buffer()
+        self.serial.write(b'\xc3')
+        response = self.serial.read(1)
+        self.ok = True
+        return  (response == b'\xc3')
+
+    def send(self, data):
+        #if self.ok:
+        #    self.serial.write(data)
+        #    self.ok = False
+        while True:
+            got = self.serial.read(1)
+            if len(got) == 0: continue
+            if got == b'a':
+                self.serial.write(data)
+                self.ok = False
+            if got == b'b':
+                self.serial.write(b'\xc1') #de-activate
+                self.ok = True
+                break
+        return self.ok
+
+    def shutdown(self):
+        self.serial.close()
+
+
+
---
+++ sample.py
@@ -0,0 +1,25 @@
+from jacq3 import *
+
+loom = Jacq3()
+connection = Comm()
+
+print('loom length: ', loom.length())
+
+print( 'initialize:', connection.initialize())
+
+#pick every 4th
+for i in range(0,360,4):
+    loom.setPick(i, True)
+
+data = loom.getPick()
+
+print('send: ', connection.send(data))
+
+#unset everything
+data = loom.getNullPick()
+
+print('send: ', connection.send(data))
+
+
+print('shutdown')
+connection.shutdown()
